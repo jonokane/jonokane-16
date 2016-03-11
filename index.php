7b32e7bf150281c8c0a502d5c65c53e905ba38cd
jonokane <jono@jonokane.com>
2016-03-10 22:16:54 -0800

    aaaalmost have that nav working and other styles are tweaked too

--- index.php
+++ index.php
@@ -22,8 +22,17 @@
 <body>
 	
 
-<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
-    <div class="navbar-inner">
+<!-- <nav class="navbar navbar-fixed-top top-bar">
+      <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+
+<div id="navbar" class="navbar-collapse collapse">
     <div class="col-md-4">
         <ul class="nav navbar-nav left">
             <li class="nav-item active fade-in one"><a href="#me" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">smile</span>Me</a></li>
@@ -31,10 +40,8 @@
             <li class="nav-item fade-in three"><a href="#contact" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">send</span>Contact</a></li>
         </ul>
         </div>
-        <div class="col-md-4">
-        <ul class="nav nav-center">
-            <li><h1 class="fancy"><a href="#">jonokane.com</a></h1></li>
-        </ul>
+        <div class="col-md-4 text-center">
+        <a class="fancy" href="#">jonokane.com</a>
         </div>
         <div class="col-md-4">
         <ul class="nav navbar-nav pull-md-right">
@@ -44,8 +51,76 @@
             <li class="nav-item"><a href="http://www.flickr.com/photos/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">flickr</span></a></li>
         </ul>
         </div>
-    </div>
-</nav>
+</div>
+</nav> -->
+
+
+
+<!-- <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bootstrap theme</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+ -->
+
+
+
+<nav class="navbar navbar-fixed-top top-bar">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand fancy" href="#">jonokane.com</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+              <li class="nav-item active fade-in one"><a href="#me" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">smile</span>Me</a></li>
+              <li class="nav-item fade-in two"><a href="#view-work" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">compose</span>Work</a></li>
+              <li class="nav-item fade-in three"><a href="#contact" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">send</span>Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="nav-item"><a href="https://www.linkedin.com/in/jonokane" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">linkedin</span></a></li>
+            <li class="nav-item"><a href="http://www.twitter.com/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">twitter</span></a></li>
+            <li class="nav-item"><a href="http://facebook.com/jonokane" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">facebook</span></a></li>
+            <li class="nav-item"><a href="http://www.flickr.com/photos/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">flickr</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+    </nav>
+
+
+
+
+
 
 
 <div class="container">
