379f85568ebce0346cd21b948e6fcd1ba0fe9c56
jonokane <jono@jonokane.com>
2016-03-14 16:22:50 -0700

    modernizing the js to work with all folio items

--- dist/js/app.js
+++ dist/js/app.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
                 speed: 5000,
                 autoplay: true,
                 autoplaySpeed: 5000,
-                infinite:true,
+                infinite: true,
                 asNavFor: '.slider-nav'
             });
             $('.slider-nav').slick({
@@ -60,134 +60,218 @@ $(document).ready(function() {
         }
     });
 
-
-
- $(function() {
-     $('.folio-fluence-app').on('click', function() {
-        event.preventDefault();
-         var $this = $(this).data('target');
-         $('#modal-box').load('folio-content/folio-fluence-app.html', function(response, status, xhr) {
-             if (status == "success") {
-                 $(this).modal('show');
-             }
-         });
-     });
- });
-
-
-
- $(function() {
-     $('.folio-shift').on('click', function() {
-        event.preventDefault();
-         var $this = $(this).data('target');
-         $('#modal-box').load('folio-content/folio-shift.html', function(response, status, xhr) {
-             if (status == "success") {
-                 $(this).modal('show');
-             }
-         });
-     });
- });
-
-  $(function() {
-     $('.folio-saratoga-speed').on('click', function() {
-        event.preventDefault();
-         var $this = $(this).data('target');
-         $('#modal-box').load('folio-content/folio-saratoga-speed.html', function(response, status, xhr) {
-             if (status == "success") {
-                 $(this).modal('show');
-             }
-         });
-     });
- });
-
-  $(function() {
-     $('.folio-telenav').on('click', function() {
-        event.preventDefault();
-         var $this = $(this).data('target');
-         $('#modal-box').load('folio-content/folio-telenav.html', function(response, status, xhr) {
-             if (status == "success") {
-                 $(this).modal('show');
-             }
-         });
-     });
- });
-
-    // $('.folio-telenav').click(function() {
-    //     $('#modal-box').modal('reveal', 'open', {
-    //         url: 'folio-content/folio-telenav.html',
-    //     });
-    // });
-
-    // $('.folio-huddleup').click(function() {
-    //     $('#modal-box').modal('reveal', 'open', {
-    //         url: 'folio-content/folio-huddleup.html',
-    //     });
-    // });
-
-    // $('.folio-breeze').click(function() {
-    //     $('#modal-box').modal('reveal', 'open', {
-    //         url: 'folio-content/folio-breeze.html',
-    //     });
-    // });
-
-    // $('.folio-intuit').click(function() {
-    //     $('#modal-box').modal('reveal', 'open', {
-    //         url: 'folio-content/folio-intuit.html',
-    //     });
-    // });
-
-    // $('.folio-ybubbles').click(function() {
-    //     $('#modal-box').modal('reveal', 'open', {
-    //         url: 'folio-content/folio-ybubbles.html',
-    //     });
-    // });
-
-    // $('.folio-thingsly').click(function() {
-    //     $('#modal-box').bootstrap('reveal', 'open', {
-    //         url: 'folio-content/folio-thingsly.html',
-    //     });
-    // });
-
-    // $('.folio-ydn').click(function() {
-    //     $('#modal-box').bootstrap('reveal', 'open', {
-    //         url: 'folio-content/folio-ydn.html',
-    //     });
-    // });
-
-    // $('.folio-ydpl').click(function() {
-    //     $('#modal-box').bootstrap('reveal', 'open', {
-    //         url: 'folio-content/folio-ydpl.html',
-    //     });
-    // });
-
-
-// jkcontact
-$(function(){
-    $("#JqAjaxForm").submit(function(e){
-       e.preventDefault();
-
-        dataString = $("#JqAjaxForm").serialize();
-
-        $.ajax({
-        type: "POST",
-        url: "emailer.php",
-        data: dataString,
-        dataType: "json",
-        success: function(data) {
-
-            if(data.email_check == "invalid"){
-                $("#contactMessage").html("<div class='errorMessage'>Sorry " + data.name + ", " + data.email + " isn't a valid email address. Give it another shot. Thanks</div>");
-            } else {
-                $("#contactMessage").html("<div class='successMessage'>Hi "+ data.name + ". Thanks for contacting me. I'll respond to you via email at " + data.email + ". Cheers.</div>");
-                $("#JqAjaxForm").hide();
-            }
-
-        }
-
-        });          
-
+    $(function() {
+        $('.folio-8t-site').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-8t-site.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
     });
-});
+
+
+    $(function() {
+        $('.folio-8t-ftux').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-8t-ftux.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+
+    $(function() {
+        $('.folio-cinematiq').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-cinematiq.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-fluence-marketing').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-fluence-marketing.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-fluence-app').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-fluence-app.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+
+
+    $(function() {
+        $('.folio-shift').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-shift.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-saratoga-speed').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-saratoga-speed.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-telenav').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-telenav.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-huddleup').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-huddleup.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-breeze').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-breeze.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-intuit').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-intuit.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-ybubbles').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-ybubbles.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-thingsly').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-thingsly.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-ydn').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-ydn.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+    $(function() {
+        $('.folio-ydpl').on('click', function() {
+            event.preventDefault();
+            var $this = $(this).data('target');
+            $('#modal-box').load('folio-content/folio-ydpl.html', function(response, status, xhr) {
+                if (status == "success") {
+                    $(this).modal('show');
+                }
+            });
+        });
+    });
+
+
+    // jkcontact
+    $(function() {
+        $("#JqAjaxForm").submit(function(e) {
+            e.preventDefault();
+
+            dataString = $("#JqAjaxForm").serialize();
+
+            $.ajax({
+                type: "POST",
+                url: "emailer.php",
+                data: dataString,
+                dataType: "json",
+                success: function(data) {
+
+                    if (data.email_check == "invalid") {
+                        $("#contactMessage").html("<div class='errorMessage'>Sorry " + data.name + ", " + data.email + " isn't a valid email address. Give it another shot. Thanks</div>");
+                    } else {
+                        $("#contactMessage").html("<div class='successMessage'>Hi " + data.name + ". Thanks for contacting me. I'll respond to you via email at " + data.email + ". Cheers.</div>");
+                        $("#JqAjaxForm").hide();
+                    }
+
+                }
+
+            });
+
+        });
+    });
 
 
 
